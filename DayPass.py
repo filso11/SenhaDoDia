9b6355d03f09d1ad1f54aac0ae52727c8eb16e4a
filso11 <filipe.luucas@gmail.com>
2023-04-16 04:13:18 -0300

    first commit

---
+++ DayPass.py
@@ -0,0 +1,16 @@
+import datetime
+import pyperclip
+
+today = datetime.datetime.now()
+
+day = today.day
+mounth = today.month
+str_year = today.strftime("%y")
+year = int(str_year)
+
+
+def todayPass(day, mounth, year):
+    return int(f"{day + year}{mounth + year}{year + year}")
+
+
+pyperclip.copy(todayPass(day, mounth, year))
